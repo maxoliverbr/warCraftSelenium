f65bcb13c11887f4100eb76cdcad3cb4f9057eb5
maxoliverbr <max.oliver@cintrax.com.br>
2023-11-14 16:29:49 -0300

    Update

--- warcrafthome.py
+++ warcrafthome.py
@@ -1,6 +1,4 @@
 """ Warcraft Selenium+Chrome SignUp automation on Github Actions """
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import logging
 import time
 from utilities.testdata import TestData
@@ -74,62 +72,45 @@ class TestWarCraftSignUp(BaseTest):
         # assert signup_text is correct
         assert signup_text == page.signup_text_expected
 
-        # 8 | birthdate | enter birthdate mm/dd/yyyy
+        # 8 | birthdate | click continue without birthdate
         page.click(page.bird_date_xpath)
 
         error_text = page.get_text(page.error_text_xpath)
 
+        # assert error message is correct
         assert error_text == page.error_text_expected
-
-        # 9 | continue | click continue
         page.click(page.dob_xpath)
 
+        # 9 | birthdate | enter birthdate mm/dd/yyyy
         page.set(page.dob_month_xpath, TestData.dob_mon)
         page.set(page.dob_day_xpath, TestData.dob_day)
         page.set(page.dob_year_xpath, TestData.dob_year)
 
-        # self.driver.find_element(By.XPATH, "//input[@name='dob-month']").send_keys(self.testdata.dob_mon)
-        # self.driver.find_element(By.XPATH, "//input[@name='dob-day']").send_keys(self.testdata.dob_day)
-        # self.driver.find_element(By.XPATH, "//input[@name='dob-year']").send_keys(self.testdata.dob_year)
-
-        # self.driver.save_screenshot('reports/ss/dob.png')
-
+        # 10 | continue | click continue
         page.click(page.continue_id)
-
         page.save_screenshot('reports/ss/continue.png')
 
+        # 11 | user info | enter first & last name
         page.set(page.first_name_id, TestData.user_first_name)
-
-        # self.driver.find_element(By.ID, "capture-first-name").send_keys(self.testdata.user_first_name)
-
-        page.save_screenshot('reports/ss/firstname.png')
-
         page.set(page.last_name_id, TestData.user_last_name)
-        # self.driver.find_element(By.ID, "capture-last-name").send_keys(self.testdata.user_last_name)
-
         page.save_screenshot('reports/ss/last.png')
 
         page.click(page.continue_id)
-        # self.driver.find_element(By.ID, "flow-form-submit-btn").click()
 
+        # 12 | user info part 2 | enter email and phone
         page.set(page.email_id, TestData.user_email)
-        # self.driver.find_element(By.ID, "capture-email").send_keys(self.testdata.user_email)
-
         page.set(page.phone_id, TestData.user_phone)
-        # self.driver.find_element(By.ID, "capture-phone-number").send_keys(self.testdata.user_phone)
 
         page.click(page.continue_id)
-        # self.driver.find_element(By.ID, "flow-form-submit-btn").click()
 
+        # 13 | assert step name
         step_name = page.get_text(page.step_name_xpath)
-        # step_name = self.driver.find_element(By.XPATH, "//h1[@class='step__title step__block']").text
 
         assert step_name == page.step_name_expect
 
         page.click(page.continue_id)
-        # self.driver.find_element(By.ID, "flow-form-submit-btn").click()
 
+        # wait page load
         time.sleep(1)
-
         page.save_screenshot('reports/ss/emailphone.png')
 
