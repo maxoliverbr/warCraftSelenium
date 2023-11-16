e9517b32e69af64d987bdf792ecd843dd72f6f24
maxoliverbr <max.oliver@cintrax.com.br>
2023-11-15 22:39:34 -0300

    Update Github Actions use cache

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -42,7 +42,7 @@ class TestWarCraftSignUp(BaseTest):
         self.driver.quit()
 
     # Firefox fails on 2560 and 1366
-    #@pytest.mark.parametrize("width,height", [(1920, 1080), (2560, 1080), (1366, 768)])
+    # @pytest.mark.parametrize("width,height", [(1920, 1080), (2560, 1080), (1366, 768)])
     @pytest.mark.parametrize("width,height", [(1920, 1080)])
     def test_warcraftsignup(self, width, height):
         """
